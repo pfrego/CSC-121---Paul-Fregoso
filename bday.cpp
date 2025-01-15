b4be31f504c2e5976786f351cfd4dc1f5ec589c2
pfrego <fp59615@mail.harpercollege.edu>
2025-01-15 14:01:22 -0600

    Add files via upload

---
+++ bday.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main () {
+    std::string name;
+    
+    std::cout<<"Enter your name?\n";
+    std::cin>>name;
+    std::cout<<"Happy Birthday to you \n";
+    std::cout<<"Happy Birthday to you \n";
+    std::cout<<"Happy Birthday Dear ";
+    std::cout<<name;
+    std::cout<<"\n";
+    std::cout<<"Happy Birthday to you \n";
+    
+    std::cout<<"    iiiiiiiiii \n";
+   std::cout<<"   |:H:a:p:p:y:| \n";
+ std::cout<<" __|___________|__ \n";
+std::cout<<"|^^^^^^^^^^^^^^^^^| \n";
+std::cout<<"|:B:i:r:t:h:d:a:y:| \n";
+std::cout<<"|                 | \n";
+std::cout<<"~~~~~~~~~~~~~~~~~~~ \n";
+
+    return 0;
+}
